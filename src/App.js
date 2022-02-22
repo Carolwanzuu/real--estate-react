7bcec0765bc13ab5d6ff9ef4a5fcfa208325292b
Carol Wanzuu <carolkyalo01@gmail.com>
2022-02-23 00:06:35 +0300

    heroes section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
 import GlobalStyle from './globalStyles';
+import Hero from './Components/Hero';
 
 function App() {
   return (
     <>
     <GlobalStyle/>
      <Navbar/>
+     <Hero/>
     </>
   );
 }
---
+++ src/data/SliderDta.js
@@ -0,0 +1,34 @@
+export const SliderData = [
+    {
+        titles:'Luxury Villa in Kiambu',
+        prices: 'ksh.90,000,000',
+        path: '/home',
+        label: 'View Home',
+        image: ImageOne,
+        alt:'House'
+    },
+    {
+        titles:'Luxury Villa in Kiambu',
+        prices: 'ksh.90,000,000',
+        path: '/home',
+        label: 'View Home',
+        image: ImageOne,
+        alt:'House'
+    },
+    {
+        titles:'Luxury Villa in Kiambu',
+        prices: 'ksh.90,000,000',
+        path: '/home',
+        label: 'View Home',
+        image: ImageOne,
+        alt:'House'
+    },
+    {
+        titles:'Luxury Villa in Kiambu',
+        prices: 'ksh.90,000,000',
+        path: '/home',
+        label: 'View Home',
+        image: ImageOne,
+        alt:'House'
+    },
+]
